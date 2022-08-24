fd2603f530577ea36d0d557a923393ef069b02d6
UO232627 - Alejandro Álvarez Solís <91067496+UO232627@users.noreply.github.com>
2022-08-24 13:31:01 +0200

    Update ejemplo peticion mongo.js

--- files/ejemplo peticion mongo.js
+++ files/ejemplo peticion mongo.js
@@ -1,16 +1,1 @@
-{
-    "_id": {
-        "$oid": "6305cc00c7c1da581ebea198"
-    },
-    "UUID": "7179d7d6-0f75-4b23-905b-16d670a7f7e1",
-    "measure": "temp",
-    "temp_threshold": "25",
-    "triggerIdentity": "f5bbd2d949a754cbaefef9afaac6fe442aa86164",
-    "ifttt_source": {
-        "id": "19148132cff536aa",
-        "url": "http://example.com/19148132cff536aa"
-    },
-    "user": {
-        "timezone": "America/Los_Angeles"
-    }
-}
+{"_id":{"$oid":"63060a13286e7f2c00f60aa1"},"uuid":"7179d7d6-0f75-4b23-905b-16d670a7f7e1","measure":"hum","triggerIdentity":"3c4532c5dd517bef9accf3850e23a265ee317496","threshold":{"$numberInt":"36"},"above_below":"<","ifttt_source":{"id":"128839915","url":"https://ifttt.com/applets/FZqk8sDt"},"user":{"timezone":"Europe/Madrid"}}
